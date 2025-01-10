bd2e6b501e224d1436693449675067de74ce4307
navyasrireddy-github <147521140+Navaysrireddy@users.noreply.github.com>
2025-01-10 17:42:40 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,96 @@
+// 1. What will be the output of the following code?
+var i = 0;
+for (i = 0; i < 3; i++) {
+    console.log(i);
+}
+
+console.log()
+
+// 2. What will the following code output?
+var i = 0;
+while (i < 2) {
+    console.log(i);
+    i++;
+}
+
+console.log()
+
+// 3. How many times will the loop execute?
+var i = 0;
+while (i < 5) {
+    i++;
+}
+
+console.log()
+
+// 4. What will this code print?
+for (var i = 0; i <= 3; i++) {
+    console.log(i);
+}
+
+console.log()
+// 5. What is the output of this code?
+var i = 0;
+do {
+    console.log(i);
+    i++;
+} while (i < 3);
+
+console.clear()
+
+// 6. How many times will the following code execute?
+var i = 0;
+do {
+    i++;
+} while (i < 10);
+
+// 7. What will be printed by this code?
+var i = 3;
+while (i > 0) {
+    console.log(i);
+    i--;
+}
+
+// 8. What is the result of the following code?
+var i = 0;
+for (; i < 3;) {
+    console.log(i);
+    i++;
+}
+
+console.log()
+
+// 9. What will this code print?
+var i = 1;
+do {
+    console.log(i);
+    i++;
+} while (i < 4);
+
+console.clear()
+
+// 10. What will be the output of the following code?
+var i = 0;
+for (i = 0; i < 5; i++) {
+    if (i == 2) {
+        break;
+    }
+    console.log(i);
+}
+
+console.clear();
+
+// 11. How many iterations will the following loop perform?
+var i = 5;
+while (i <= 5) {
+    i++;
+}
+
+console.log()
+
+// 12. What will this code print?
+var i = 0;
+for (var j = 0; j < 2; j++) {
+    i++;
+}
+console.log(i);
